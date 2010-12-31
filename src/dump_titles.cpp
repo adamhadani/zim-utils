82569d61a7ca4addf3ca253ff358818c1b69507b
Adam Ever-Hadani <adamhadani@gmail.com>
2010-12-31 15:53:29 -0800

    dump_titles namespace filtering

--- src/dump_titles.cpp
+++ src/dump_titles.cpp
@@ -1,8 +1,13 @@
 /**
- * dump_titles.cpp
- *
- * Simple utility to dump all page titles
- * to a text file from a .zim corpus
+  dump_titles.cpp
+
+  Simple utility to dump all page titles
+  to a text file from a .zim corpus.
+
+  Optionally, one can supply a namespace to limit articles to, e.g for wikipedia release version,
+  there's the articles namespace ('A') and the images one ('I'), and typically one would only be interested
+  in the articles for textual dumps.
+
  */
 
 #include <iostream>
@@ -16,23 +21,27 @@ using namespace std;
 using zim::File;
 
 void usage() {
-    fprintf(stderr, "USAGE: dump_titles <zim_file>\n\n");
+    fprintf(stderr, "USAGE: dump_titles <zim_file> <namespace>\n\n");
 }
 
 int main(int argc, char** argv) {
-    if (argc == 1) {
+    if (argc < 3) {
         usage();
         return -1;
     }
 
     char* fname = argv[1];
+    char filterNS = argv[2][0];
 
-    cout << "Reading zim file: " << fname << endl;
+    cerr << "Reading zim file: " << fname << ", Namespace: " << filterNS << endl;
     zim::File zimFile(fname);
 
     zim::File::const_iterator it;
     
-    for (it = zimFile.begin(); it != zimFile.end(); it++) {
+    for (it = zimFile.begin(); it != zimFile.end(); ++it) {
+		if ( it->getNamespace() != filterNS )
+			continue;
+
         cout << it->getTitle() << endl;
     }
 
